0fbcf1c3ecb85fe57d23a09a6cc03d35ccbee07a
GhostSlayer <aksuplayz1337@gmail.com>
2020-02-23 22:27:57 +0200

    Initial Commit

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  CLIENT_ID: '"346014632395407362"',
-  REDIRECT_URI: '"http://localhost:8080/auth"',
-  BLADEY_API_ROOT: '"http://localhost:8000/api"'
-})
--- src/utils/BrowserUtils.js
+++
@@ -1,7 +0,0 @@
-function supportWebP () {
-  const canvas = typeof document === 'object' ? document.createElement('canvas') : {}
-  canvas.width = canvas.height = 1
-  return canvas.toDataURL ? canvas.toDataURL('image/webp').indexOf('image/webp') === 5 : false
-}
-
-export default { supportWebP }
